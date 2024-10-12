e8927217c25a31f862c23f8c713589cf41fb7726
Murphie Pettyjohn <98448438+Mothpy@users.noreply.github.com>
2024-10-11 18:58:38 -0700

    initial upload

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive)
+VALUES 
+  ('Frida', 'Kahlo', '1907-07-06', false),
+  ('Yayoi', 'Kusama', '1929-03-22', true),
+  ('Jean-Michel', 'Basquiat', '1960-12-22', false);
